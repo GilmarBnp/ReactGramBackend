ae55ec486c3047cc6fa9f05ecf1fd6319b014c8e
GilmarBNP <Mestrevii@hotmail.com>
2023-01-17 13:25:49 -0300

    user controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,24 +1,55 @@
-const User = require("../models/User");
+    const User = require("../models/User");
 
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+    const bcrypt = require("bcryptjs");
+    const jwt = require("jsonwebtoken");
 
-const jwtSecret = process.env.JWT_SECRET;
+    const jwtSecret = process.env.JWT_SECRET;
 
-//Generate user token
+    //Generate user token
 
-const generateToken = (id) =>{
-    return jwt.sign({id}, jwtSecret, {
-        expiresIn: "7d",
+    const generateToken = (id) =>{
+        return jwt.sign({id}, jwtSecret, {
+            expiresIn: "7d",
+        });
+    }
+
+    //Register user and sign in
+
+    const register = async(req, res) => {
+        const {name, email, password} = req.body
+    
+    //check if user exist
+    const user = await User.findOne({email});
+
+    if(user) {
+      res.status(422).json({errors: ["Por favor, utilize outro e-mail."]})
+      return
+    }
+
+    //Generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    //Create user
+    const newUser = User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    //If user was created succesfully, return the token
+    if(!newUser) {
+        res.status(422).json({errors: ["Houve o erro, por favor tente mais tarde."]})
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id),
     });
-}
+    
+    }
 
-//Register user and sign in
-
-const register = async(req, res) => {
-    res.send("Registro")
-}
-
-module.exports = {
-    register,
-}
+    module.exports = {
+        register,
+    }
